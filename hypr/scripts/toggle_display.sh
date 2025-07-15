ca1b246fc7b345394bde031eed0c6be2919bee04
Ali Alakbar <161264644+Order52@users.noreply.github.com>
2025-07-15 18:39:27 +0300

    Create toggle_display.sh

---
+++ hypr/scripts/toggle_display.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+STATUS=$(hyprctl monitors -j | jq -r '.[] | select(.name=="eDP-2") | .dpmsStatus')
+
+if [[ "$STATUS" == "true" ]]; then
+    hyprctl dispatch dpms off eDP-2
+else
+    hyprctl dispatch dpms on eDP-2
+fi
